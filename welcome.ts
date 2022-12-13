ead56edfe623fbd40bf7983e802b512d7036cf8c
Phaneendra <phaneendra_vvk@siemon.com>
2022-12-13 11:14:16 +0530

    new branch commit

--- welcome.ts
+++ welcome.ts
@@ -1,1 +1,35 @@
 This is the sample file.
+
+
+//Git initialization
+git init
+
+
+//add the files towards repp
+git add . 
+
+//will commit the changes in to your local repo
+git commit -m "Initial Push comment"
+
+
+
+//this will clone - push my code into remote repository
+git push 
+
+
+//get the skeleton of repo
+git fetch
+
+
+//get latest code into your local
+git pull
+
+
+//to know the existing branch name
+git brach
+
+//to see all branches
+git branch -a
+
+//create to branch from master
+git checkout -b /feature/ml1
